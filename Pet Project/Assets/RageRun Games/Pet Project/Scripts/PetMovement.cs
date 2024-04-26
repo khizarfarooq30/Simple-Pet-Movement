96a1049de741bc062ca06990c1123d2d0b08127a
Muhammad Khizar <khizarfarooq30@gmail.com>
2024-04-26 05:55:37 +0500

    remove unused packages

--- Pet Project/Assets/RageRun Games/Pet Project/Scripts/PetMovement.cs
+++ Pet Project/Assets/RageRun Games/Pet Project/Scripts/PetMovement.cs
@@ -1,71 +1,86 @@
-using System;
 using UnityEngine;
 
 public class PetMovement : MonoBehaviour
 {
-       public Transform followTransform;
    
-       [SerializeField] private float maxSpeed = 3f;
-       [SerializeField] private float maxDistance = 3f;
-       [SerializeField] private float stopDistance = 1f;
-     
-       [SerializeField] private bool maintainInitialYPosition = true;
-       
-       private Vector3 velocity;
-       
-       private float acceleration;
-       private float previousAcceleration;
-       
-       private float speedCap = 0;
-       private float initialYPosition;
-       
-       public bool EnableFollow { get; set; }
+    public Transform leaderTransform; 
 
-       private void Start()
-       {
-           initialYPosition = transform.position.y;
-           acceleration = maxSpeed / (maxDistance * 2f);
-           previousAcceleration = acceleration;
-           Debug.Log(acceleration);
-       }
+    [SerializeField] private float moveSpeed = 3f;
+    [SerializeField] private float maxDistance = 3f;
+    [SerializeField] private float stopDistance = 1f;
 
-       private void Update()
-       {
-           HandlePetMovement();
-       }
+    [SerializeField] private Vector3 followOffset = new Vector3(1f, 0f, 1f);
+    
+    [SerializeField] private bool ignoreOffset;
+    [SerializeField] private bool randomizeAutoOffset;
+    [SerializeField] private bool enablePetMoveToGroundWhenIdle;
+    [SerializeField] private bool alwaysStayOnGround;
+    
+    [SerializeField] private Animator animator;
+    
+     private Transform followTransform;
 
-       private void HandlePetMovement()
-       {
-           if (followTransform == null) return;
-           
-           Vector3 targetPos = followTransform.position;
-           Vector3 position = transform.position;
+    private Vector3 targetPos;
+    private Vector3 velocity;
 
-           speedCap = (Mathf.Clamp(Vector3.Distance(transform.position, followTransform.position), stopDistance, maxDistance) - stopDistance) * maxSpeed;
+    private float speedCap = 0;
+    private float initialYPosition;
+    
+    private Leader leader;
 
-           Vector3 force = (targetPos + (maintainInitialYPosition ? Vector3.up * (initialYPosition - transform.position.y) : Vector3.zero) - position).normalized * acceleration;
-           velocity = Vector3.ClampMagnitude(velocity + force, speedCap);
-           position += velocity * Time.deltaTime;
-           transform.position = position;
-           
-           transform.LookAt(followTransform);
+    public bool EnableFollow { get; set; }
 
-           if (Math.Abs(previousAcceleration - acceleration) > 0.025f)
-           {
-               acceleration = maxSpeed / (maxDistance * 2f);
-               previousAcceleration = acceleration;
-           }
-         
-       }
+    private void Start()
+    {
+        initialYPosition = transform.position.y;
 
-       public void SetFollowTarget(Transform target)
-       {
-           followTransform = target;
-           EnableFollow = true;
-       }
-       
-       public bool IsFollowingTarget()
-       {
-           return followTransform != null;
-       }
+        leaderTransform = GameObject.FindWithTag("Player").transform;
+        
+        leader = leaderTransform.GetComponent<Leader>();
+        
+        if (randomizeAutoOffset)
+        {
+            followOffset = new Vector3(Random.Range(-2f, 2f), 0f, Random.Range(-2f, 2f));
+        }
+    }
+
+    void Update()
+    {
+        if (!EnableFollow) return;
+
+        if (ignoreOffset)
+        {
+            targetPos = followTransform.position + new Vector3(0, enablePetMoveToGroundWhenIdle && leader.MoveVector.magnitude < 0.1f ? 0f : followOffset.y, 0);
+        }
+        else
+        {
+            targetPos = leaderTransform.position + new Vector3(followOffset.x, enablePetMoveToGroundWhenIdle && leader.MoveVector.magnitude < 0.1f ? initialYPosition : followOffset.y, followOffset.z);
+        }
+        
+        Vector3 position = transform.position;
+        float speedCap = (Mathf.Clamp(Vector3.Distance(position, targetPos), stopDistance, maxDistance) - stopDistance) * moveSpeed;
+        Vector3 direction = (targetPos - position).normalized;
+
+        velocity = Vector3.ClampMagnitude(velocity + direction, speedCap);
+        position += velocity * Time.deltaTime;
+        transform.position = position;
+
+        transform.LookAt(targetPos);
+
+        if (animator != null)
+        {
+            animator.SetBool("Moving", velocity.magnitude > 0.1f);
+        }
+    }
+
+    public void SetFollowTarget(Transform target)
+    {
+        followTransform = target;
+        EnableFollow = true;
+    }
+
+    public bool IsFollowingTarget()
+    {
+        return followTransform != null;
+    }
 }
