249c5fea15abd77eb666b8a2fb886f9abd551a32
Muhammad Khizar <khizarfarooq30@gmail.com>
2024-04-26 05:23:21 +0500

    Implement pet movement with offsets

--- Pet Project/Assets/RageRun Games/Pet Project/Scripts/Leader.cs
+++ Pet Project/Assets/RageRun Games/Pet Project/Scripts/Leader.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 public class Leader : MonoBehaviour
 {
    [SerializeField] private float moveSpeed = 5f;
+   [SerializeField] private Animator animator;
+   
+   [SerializeField] private bool useUpwardMovement;
 
    private float horizontal;
    private float vertical;
-
+   private float up;
+   
    private List<Transform> followTransforms = new List<Transform>();
+
+   public Vector3 MoveVector { get; set; }
 
    private void Start()
    {
@@ -17,12 +23,27 @@ public class Leader : MonoBehaviour
 
    private void Update()
    {
-      horizontal = Input.GetAxis("Horizontal");
-      vertical = Input.GetAxis("Vertical");
-      float up = Input.GetKey(KeyCode.UpArrow) ? 1 : Input.GetKey(KeyCode.DownArrow) ? -1 : 0;
+      horizontal = Input.GetAxisRaw("Horizontal");
+      vertical = Input.GetAxisRaw("Vertical");
 
-      Vector3 moveVec = new Vector3(horizontal, up, vertical);
-      transform.position += moveVec * (moveSpeed * Time.deltaTime);
+      if (useUpwardMovement)
+      {
+         up = Input.GetKey(KeyCode.UpArrow) ? 1 : Input.GetKey(KeyCode.DownArrow) ? -1 : 0;
+      }
+      
+      MoveVector = new Vector3(horizontal, up, vertical).normalized;
+      transform.position += MoveVector * (moveSpeed * Time.deltaTime);
+      
+      if (MoveVector.magnitude > 0.1f)
+      {
+         // smooth rotation
+         transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(MoveVector), Time.deltaTime * 10f);
+      }
+      
+      if (animator != null)
+      {
+         animator.SetBool("Moving", MoveVector.magnitude > 0.1f);
+      }
    }
 
    private void OnTriggerEnter(Collider other)
